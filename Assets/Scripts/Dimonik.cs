2f9b96c803f5f871babf6334c206c8d1f3cea049
Alberto Ramos <aramos@egogames.com>
2020-11-23 21:34:01 +0100

    Idle and walking animations done for Dimonik

---
+++ Assets/Scripts/Dimonik.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dimonik : MonoBehaviour
+{
+    public float m_speed;
+
+    private SpriteRenderer m_renderer;
+    private Rigidbody2D m_rigidBody;
+    private Animator m_animator;
+    private Vector2 m_moveVector = new Vector2();
+
+    private void Start()
+    {
+        m_renderer = GetComponent<SpriteRenderer>();
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        m_moveVector.x = Input.GetAxisRaw("Horizontal");
+        m_moveVector.y = Input.GetAxisRaw("Vertical");
+
+        if (m_moveVector.x == 0 && m_moveVector.y == 0)
+        {
+            m_animator.SetBool("Moving", false);
+        }
+        else
+        {
+            m_animator.SetBool("Moving", true);
+        }
+
+        if (m_moveVector.y == 1)
+        {
+            m_animator.SetBool("FacingForward", false);
+        }
+        else if (m_moveVector.y == -1)
+        {
+            m_animator.SetBool("FacingForward", true);
+        }
+
+        if (m_moveVector.x == 1)
+        {
+            m_renderer.flipX = false;
+        }
+        else if (m_moveVector.x == -1)
+        {
+            m_renderer.flipX = true;
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        m_rigidBody.MovePosition(m_rigidBody.position + m_moveVector * m_speed * Time.fixedDeltaTime);
+    }
+
+}
